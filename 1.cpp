5f47b4a3ae0376cd68dfdc11b87a5fcfcf1f8d53
Yatoshk <48162785+Yatoshk@users.noreply.github.com>
2019-11-05 16:34:23 +0400

    1 problem is done

---
+++ 1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int const n = 10;
+int result = 0;
+int main()
+{
+	int array[n];
+	for (int i = 1; i < n; i++)
+	{
+		array[i - 1] = i;
+		if (array[i - 1] % 3 == 0 || array[i - 1] % 5 == 0)
+		{
+			result += array[i - 1];
+		}
+	}
+	cout << result;
+	cout << endl;
+	system("pause");
+	return 0;
+}
+

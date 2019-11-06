b718d5c6a78335ebd168b01b86e3f43d49721164
Yatoshk <48162785+Yatoshk@users.noreply.github.com>
2019-11-06 21:18:57 +0400

    2,6 is done

---
+++ 2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+int const i = 3999999;
+int result = 0;
+int Fib(int n)
+{
+	if (n == 0)
+		return 1;
+	else if (n == 1)
+		return 2;
+	n = Fib(n - 1) + Fib(n - 2);
+	return n;
+}
+
+int main()
+{
+	for (int k = 0; k < i; k++)
+	{
+		if (Fib(k) % 2 == 0)
+		{
+			result += Fib(k);
+		}
+	}
+	cout << result;
+}
---
+++ 6.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int SumOfSquares, SquaresOfSum;
+int box = 0;
+int main()
+{
+	for (int i = 1; i <= 100; i++)
+	{
+		SumOfSquares += i * i;
+	}
+	for (int i = 1; i <= 100; i++)
+	{
+		box += i;
+	}
+	SquaresOfSum += box * box;
+	cout << SquaresOfSum - SumOfSquares;
+}

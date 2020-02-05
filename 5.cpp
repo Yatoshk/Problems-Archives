93653ffdbe6bfa9122777772fa49c4bbdd0de41f
Yatoshk <48162785+Yatoshk@users.noreply.github.com>
2020-02-05 23:43:35 +0400

    Add files via upload
    Add 5.cpp

---
+++ 5.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int serchNumber = 10000000000;
+int counting, x = 20;
+int main()
+{
+	for (int n = 1; n <= 10000000000; n++)
+	{
+		for (int i = 1; i <= x; i++)
+		{
+			if (n % i == 0)
+				counting++;
+		}
+		if (counting == x)
+		{
+		cout << n << endl;
+			if (n < serchNumber)
+				serchNumber = n;
+		}
+		counting = 0;
+	}
+	cout << "Smallest numb: " << serchNumber << endl;
+}

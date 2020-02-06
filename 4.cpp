dec682c55372d5a8e72dd3dce77347412f07eae1
Yatoshk <48162785+Yatoshk@users.noreply.github.com>
2020-02-06 16:12:39 +0400

    Add files via upload
    Add 4.cpp

---
+++ 4.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int x;
+int y = 0;
+
+int Inversion(int n)
+{
+	int l = 0;
+	while (n != 0)
+	{
+		l = l * 10 + (n % 10);
+		n /= 10;
+	}
+	return l;
+}
+
+int main()
+{
+	for (int i = 100; i < 1000; i++)
+	{
+		for (int j = 100; j < 1000; j++)
+		{
+			x = i * j;
+			if ((y < x) && (Inversion(x) == x))
+			{
+				y = x;
+				cout << y << endl;
+			}
+		}
+	}
+}

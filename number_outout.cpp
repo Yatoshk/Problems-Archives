d432941dd36674d5093ebd3205a91dde317b031f
Yatoshk <48162785+Yatoshk@users.noreply.github.com>
2020-02-19 18:42:12 +0400

    Add files via upload
    some programm

---
+++ number_outout.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream> 
+#include <ctime> 
+using namespace std;
+int min, max, n;
+int i = 0;
+int main(void)
+{
+    cout << "Enter the number of digits in the number: ";
+    cin >> n;
+    int *x = new int[n];
+    cout << "Enter the max, min: ";
+    cin >> max;
+    cin >> min;
+    srand(time(NULL));
+    int a = min + rand() % (max - min);
+    cout << "Random number: " << a << endl;
+    while (i < n)
+    {
+        x[i] = a % 10;
+        a /= 10;
+        i++;
+    }
+    for (int j = n; j > 0; j--)
+    {
+        cout << x[j -1];
+        if (j > 1)
+        { cout << ", "; }
+    }
+    return 0;
+}
+
+
+
+

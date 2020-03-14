646cc615a2e59ac6346cff2c66b0fe6ca6bf22c3
Yatoshk <48162785+Yatoshk@users.noreply.github.com>
2020-03-14 12:15:40 +0400

    Add files via upload
    14, 16

---
+++ 14.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream> 
+using namespace std;
+long long Mvalue = 0;
+long Mlength = 0;
+long l(long long n)
+{
+	long i = 1;
+	while (n != 1)
+	{
+		if (n % 2 == 0)
+		{
+			n /= 2;
+		}
+		else
+		{
+			n = 3 * n + 1;
+		}
+		i++;
+	}
+	return i;
+}
+int main(void)
+{
+	for (long long i = 2; i < 1000000; i++)
+	{
+		long length = l(i);
+		if (length > Mlength) {
+			Mlength = length;
+			Mvalue = i;
+		}
+	}
+	cout << "Biggest chain: " << Mvalue << ": "<< Mlength<< endl;
+	return 0;
+}
+
---
+++ 16.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+unsigned long long counting = 0;
+
+void LastTen(unsigned long long n)
+{
+	while (n != 0)
+	{
+		counting += n % 10;
+		n /= 10;
+	}
+	cout << counting << endl;
+}
+
+int main()
+{
+	unsigned long long m = exp2(34);
+	LastTen(m);
+	return 0;
+}
+
